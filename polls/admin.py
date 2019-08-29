07a2bf441004145438709944d80da9f7570c89b8
academey <academey@gmail.com>
2019-08-29 15:12:01 +0900

    Add customized templates & Add boolean setting for admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
+# 이렇게 할수도 있고,
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # admin list 에서 field 들 순서를 고를 수 있음
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # 이거 있으면 옆에 필터 사이드 바가 생김. DateTimeField 여서 적절한 필터 옵션을 제공함
+    list_filter = ['pub_date']
+
+    # 이거 있으면 위에 검색창이 생김. question_text에 LIKE 쿼리를 사용함
+    search_fields = ['question_text']
+
+    # admin detail 에서 field 들에 대해 더 이쁘게 볼 수 있음
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+# Choice를 관리할 수 있게 하자
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,11 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    # 이걸 추가해주면 admin에서 정렬 및 Boolean 값으로 보여질 수 있음.
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
